49c703b3de5af1b269123f3a6752854398efdbed
carlosmorin78 <carlosmorin78@gmail.com>
2020-03-21 17:31:09 -0600

    user views added new, and show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  default_scope { order(:created_at) }
+  
+  validates :name, :last_name, :email, :phone, presence: true
+
+  def full_name
+  	"#{name} #{last_name}"
+  end
 end
