9ece718e0c262efd20f7d2c7f55d8419de63f6ba
carlosmorin78 <carlosmorin78@gmail.com>
2020-03-23 11:05:12 -0600

    mirations to add new model and remove others filds to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_233638) do
+ActiveRecord::Schema.define(version: 2020_03_23_170036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+  create_table "general_information", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
-    t.string "phone"
+    t.datetime "born_date"
+    t.string "alias"
+    t.string "level_studies"
+    t.string "school_name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_general_information_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", default: "", null: false
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_03_21_233638) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "general_information", "users"
 end
