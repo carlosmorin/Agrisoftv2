d72a8b264aa7e6cb3f7791da6c4bccfcdd942759
carlosmorin78 <carlosmorin78@gmail.com>
2020-03-25 07:18:30 -0600

    input mask implementation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,23 +8,25 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("admin-lte")
+require('jquery')
 
+var inputmask = require('inputmask');
 import "bootstrap"
 import "../stylesheets/application"
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
+
 
 document.addEventListener("turbolinks:load", () => {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+
+	var object = document.getElementsByClassName("mask") 
+	var im = new Inputmask();
+	im.mask(object);
 })
 
-
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-import "controllers"
