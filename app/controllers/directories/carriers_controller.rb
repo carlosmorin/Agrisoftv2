df1dc3caeb1b29bdd4090571ee48c4829fbe3867
gibranhdz <gerrardmagallanes@gmail.com>
2020-04-16 09:49:58 -0500

    the transporters catalog is now working

--- app/controllers/directories/carriers_controller.rb
+++ app/controllers/directories/carriers_controller.rb
@@ -5,8 +5,25 @@ module Directories
   	add_breadcrumb "Transportistas", :directories_carriers_path
 
   	def index
-  		@carriers = Carrier.all
+  		@carriers = Carrier.paginate(page: params[:page], per_page: 12)
+    	search if params[:q].present?
   	end
+
+  private
+
+  def search
+    q = Regexp.escape(params[:q])
+    
+    @carriers = @carriers.where("concat(name, ' ', email, ' ', phone, ' ', contact_name) ~* ?", q)
+  end
+
+	def package_params
+    params.require(:carrier).permit(:name, :email, :phone, :contact_name)
+  end
+
+  def set_object
+    @carrier = Carrier.find(params[:id])
+  end
 
   end
 end
