f1311eb9355a50bf346cb922a175176dd02d7b24
carlosmorin78 <carlosmorin78@gmail.com>
2020-04-28 10:28:30 -0500

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Agrisoft
 
     config.assets.enabled = true  
     config.assets.paths << "#{Rails.root}/app/assets/fonts"  
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
