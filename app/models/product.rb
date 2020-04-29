2c2afb544e546993c9942292b3854a2942f993cf
carlosmorin78 <carlosmorin78@gmail.com>
2020-04-29 15:05:54 -0500

    product name corrections

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,6 +20,6 @@ class Product < ApplicationRecord
     size_name = size.short_name.present? ? size.short_name : size.name
     quality_name = quality.short_name.present? ? quality.short_name : quality.name
     self.name = "#{crop.name} #{color.name} #{quality_name} #{size_name} 
-      #{package.name} #{client_brand.name}"
+      #{package.name} #{client_brand.name} #{weight} #{unit_meassure} "
   end
 end
