5c1ae9ca19fdb24814fdb031fe4300754433da9f
carlosmorin78 <carlosmorin78@gmail.com>
2020-06-11 14:46:12 -0500

    validate shipment

--- app/models/shipments_product.rb
+++ app/models/shipments_product.rb
@@ -2,7 +2,7 @@ class ShipmentsProduct < ApplicationRecord
   belongs_to :shipment, optional: true
   belongs_to :product
   validates :product_id, :quantity, presence: true
-	belongs_to :productable, polymorphic: true
+	belongs_to :productable, polymorphic: true, optional: true
   
   enum unit_meassure: { lbs: 0, kg: 1, granel: 2, bulto: 3, caja: 4 }
 end
