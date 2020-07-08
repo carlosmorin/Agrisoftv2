340e9dc95fc77318d13dccb03f2a3b3f1975832c
Jose Morin <carlosmorin78@gmail.com>
2020-07-08 10:59:07 -0500

    Some corrections and test

--- spec/controllers/sizes_controller_spec.rb
+++ spec/controllers/sizes_controller_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
-require 'pry'
-RSpec.describe SizesController, type: :controller do
-	login_user
 
-	let(:valid_attributes) {
-   { name: "Hola", short_name: "This is a test description" }
-  }
+RSpec.describe SizesController, type: :controller do
 
   describe "GET #index" do
+		login_user
     it "returns a success response" do 
     	2.times do
       	size = create(:size)
