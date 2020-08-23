4f91e7b795f96cdd595c9ff66b863a58e0405ccc
Jose Morin <carlosmorin78@gmail.com>
2020-08-23 08:06:24 -0500

    Carrier detail and Contacts in the carri show

--- app/controllers/concerns/delivery_addresses_breadcrumbs.rb
+++ app/controllers/concerns/delivery_addresses_breadcrumbs.rb
@@ -1,32 +1,32 @@
 module DeliveryAddressesBreadcrumbs
-	extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-	included do
+  included do
     add_breadcrumb "CRM", :crm_root_url
     add_breadcrumb "Clientes", :crm_clients_url
-	end
+  end
 
-	def build(action_name)
-		case action_name	
-		when "new", "create"
-			breadrcumbs_new
-		when "edit", "update"
-			breadrcumbs_edit
-		end
-	end
+  def build(action_name)
+    case action_name
+    when "new", "create"
+      breadrcumbs_new
+    when "edit", "update"
+      breadrcumbs_edit
+    end
+  end
 
-	def breadrcumbs_new
-		add_breadcrumb @client.name.upcase, crm_client_path(@client, tab: :addresses)
-		add_breadcrumb "Direcciones de entrega", crm_client_path(@client, tab: :addresses)
-		add_breadcrumb "Nuevo"
-	end
+  def breadrcumbs_new
+    add_breadcrumb @client.name.upcase, crm_client_path(@client, tab: :addresses)
+    add_breadcrumb "Direcciones de entrega", crm_client_path(@client, tab: :addresses)
+    add_breadcrumb "Nuevo"
+  end
 
-	def breadrcumbs_edit
-		add_breadcrumb @client.name.upcase, crm_client_path(@client, tab: :contacts)
-		add_breadcrumb "Direcciones de entrega", crm_client_path(@client, tab: :addresses)
-		add_breadcrumb @address.name.upcase 
-		add_breadcrumb "Editar"
-	end
+  def breadrcumbs_edit
+    add_breadcrumb @client.name.upcase, crm_client_path(@client, tab: :contacts)
+    add_breadcrumb "Direcciones de entrega", crm_client_path(@client, tab: :addresses)
+    add_breadcrumb @address.name.upcase 
+    add_breadcrumb "Editar"
+  end
 
 	def set_client
 		client_id = params[:client_id].present? ? 
@@ -36,5 +36,5 @@ module DeliveryAddressesBreadcrumbs
 
 	def set_breadcrumb
 		build(action_name)
-	end		
+	end
 end
