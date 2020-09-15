57fef552fc67224ca1ffce200f612876bbab8001
Jose Morin <carlosmorin78@gmail.com>
2020-09-15 13:00:19 -0500

    Some changes

---
+++ app/javascript/controllers/crm/sales_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from "stimulus"
+import swal from 'sweetalert';
+const Axios = require('axios');
+const toastr = require('toastr');
+
+export default class extends Controller {
+  static targets = [ "output" ]
+
+  connect() {
+
+  }
+  confirmCancelSale(event){
+    event.preventDefault();
+    var saleNumber = event.currentTarget.getAttribute('data-saleNumber')
+    var sale_id = event.currentTarget.getAttribute('data-id')
+    var cancel = event.currentTarget.getAttribute('data-cancel')
+    swal({
+      title: "Requiere confirmación!",
+      text: `Deseas Cancelar la venta ${saleNumber} ?`,
+      buttons: true,
+      buttons: ['Cancelar', 'Aceptar']
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        this.cancelSale(sale_id, cancel)
+      } 
+    });
+  }
+
+  cancelSale(id, cancel){
+    var url = `sales/${id}/cancel`
+    Axios({
+      method: 'patch',
+      url: url,
+      headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')},
+      data: { cancel: cancel }
+    })
+    .then(function (response) {
+      if (response['status'] == 204) {
+        toastr.success('Operación realizada con exito!', {timeOut: 1000})
+        setTimeout(function(){ Turbolinks.visit(window.location, { action: 'replace' }) }, 1000);
+      }
+    });
+  }
+}
--- app/javascript/controllers/crm/sales_orders_controller.js
+++ app/javascript/controllers/crm/sales_orders_controller.js
@@ -1,4 +1,6 @@
 import { Controller } from "stimulus"
+const Axios = require('axios');
+const toastr = require('toastr');
 
 export default class extends Controller {
   static targets = [ "appoinmentContainer" ]
@@ -13,7 +15,45 @@ export default class extends Controller {
   	}else{
   		this.appoinmentContainerTarget.classList.add('d-none')
   	}
-
   }
 
+ confirmCancelOV(e){
+    e.preventDefault()
+    var ovNumber = e.currentTarget.getAttribute('data-ovNumber')
+    var ov_id = e.currentTarget.getAttribute('data-id')
+    var cancel = e.currentTarget.getAttribute('data-cancel')
+    var msg = ''
+    if (cancel == '0') {
+      msg = 'Deseas activar la Orden de venta'
+    }else{
+      msg = 'Deseas cancelar la Orden de venta'
+    }
+    swal({
+      title: "Requiere confirmación!",
+      text: `${msg} ${ovNumber}?`,
+      buttons: true,
+      buttons: ['Cancelar', 'Aceptar']
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        this.cancelOV(ov_id, cancel)
+      } 
+    });
+  }
+
+  cancelOV(id, cancel){
+    var url = `/crm/sales_orders/${id}/cancel`
+    Axios({
+      method: 'patch',
+      url: url,
+      headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')},
+      data: { cancel: cancel }
+    })
+    .then(function (response) {
+      if (response['status'] == 204) {
+        toastr.success('Operación realizada con exito!', {timeOut: 1000})
+        setTimeout(function(){ Turbolinks.visit(window.location, { action: 'replace' }) }, 1000);
+      }
+    });
+  }
 }
