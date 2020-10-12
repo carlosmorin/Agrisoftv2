2bfd5ee960a042c913c44c2f0c8908fc45d89367
Jose Morin <carlosmorin78@gmail.com>
2020-10-12 08:57:13 -0500

    MIgration changed

--- db/migrate/20200919144608_add_extra_fiedls_to_contracts_expenses.rb
+++ db/migrate/20200919144608_add_extra_fiedls_to_contracts_expenses.rb
@@ -1,6 +1,6 @@
 class AddExtraFiedlsToContractsExpenses < ActiveRecord::Migration[6.0]
   def change
-    add_reference :contracts_expenses, :currency, null: false, foreign_key: true
+    add_reference :contracts_expenses, :currency, null: true, foreign_key: true
     add_column :contracts_expenses, :price, :decimal
   end
 end
