7ec9fd52ac1b6aeeaa9a568d3797d99125008d7b
Jose Morin <carlosmorin78@gmail.com>
2020-11-08 18:57:30 -0600

    SOme changes

--- db/migrate/20201109004328_set_shipment_at_date_from_shipments.rb
+++ db/migrate/20201109004328_set_shipment_at_date_from_shipments.rb
@@ -2,7 +2,7 @@ class SetShipmentAtDateFromShipments < ActiveRecord::Migration[6.0]
   def change
 		shipments = Shipment.where(shipment_at: nil)
 		shipments.each do |shipment|
-			shipment.update(shipment_at: shipment.created_at)
+			shipment.update_attribute('shipment_at', shipment.created_at)
 		end
   end
 end
